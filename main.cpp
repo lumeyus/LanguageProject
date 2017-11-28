70501e24dc664061d8e5d5206210fba91c2370fe
Unknown <luisfibanez@my.uri.edu>
2017-11-28 16:39:53 -0500

    another slight main change

--- main.cpp
+++ main.cpp
@@ -8,8 +8,8 @@ using namespace std;
 int main(int argc, char *argv[])
 {
     if (argc != 2){exit(EXIT_FAILURE);}
-    map<string, int> inTris = trigrams(argv[1])
-    map<string, int> everyTri = allTris()
+    map<string, int> inTris = trigrams(argv[1]);
+    map<string, int> everyTri = allTris();
     string freq = frequencies(inTris, everyTri);
     cout << freq << endl;
 }
