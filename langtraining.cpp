80c7af34effcc561f881bcf0b6203cdb72fed0eb
Unknown <luisfibanez@my.uri.edu>
2017-11-28 12:35:43 -0500

    fixed langtraining header, started cpp

---
+++ langtraining.cpp
@@ -0,0 +1,13 @@
+#include "langtraining.h"
+#include <vector>
+#include <string>
+
+using namespace std;
+
+map<string trigram, int frequency> trigrams(string input)
+{
+    for (int i = 0; i < input.length() - 3; i+=3)
+    {
+        
+    }
+}
--- langtraining.h
+++ langtraining.h
@@ -8,12 +8,12 @@ using namespace std;
 /*
 Returns a map of trigrams with key being the trigram and value the frequency of that trigram
 */
-map<string trigram, int frequency> trigrams;
+map<string trigram, int frequency> trigrams(string input);
 
 /*
 Returns a map of all trigrams ("   " to "zzz")
 */
-map<string trigram, int frequency> allTris;
+map<string trigram, int frequency> allTris();
 
 /*
 Returns a string with frequencies of each trigram ("   " to "zzz") seperated by spaces
