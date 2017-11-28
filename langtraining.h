ab5a0711c596c03614c4e68fde9cece9561f50ec
Unknown <luisfibanez@my.uri.edu>
2017-11-28 10:52:18 -0500

    Set up langtraining header

--- langtraining.h
+++ langtraining.h
@@ -1,8 +1,23 @@
 #ifndef _langtraining_h
 #define _langtraining_h
+#include <map>
+#include <string>
 
 using namespace std;
 
+/*
+Returns a map of trigrams with key being the trigram and value the frequency of that trigram
+*/
+map<string trigram, int frequency> trigrams;
 
+/*
+Returns a map of all trigrams ("   " to "zzz")
+*/
+map<string trigram, int frequency> allTris;
+
+/*
+Returns a string with frequencies of each trigram ("   " to "zzz") seperated by spaces
+*/
+string frequencies(map trigrams);
 
 #endif
