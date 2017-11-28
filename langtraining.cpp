bc84b7429f37294308455a7c20ae81c51312e547
Unknown <luisfibanez@my.uri.edu>
2017-11-28 16:27:49 -0500

    Finished allTris and trigrams functions, slight change to main

--- langtraining.cpp
+++ langtraining.cpp
@@ -11,18 +11,33 @@ alphabet[0] = ' ';
 for (i = 1; i < 27; i++)
     alphabet[i] = 'a' + (i - 1);
 
-
+// Iterator map to loop through trigram map of input
 map<string, int>::iterator it;
 
+/*
+Function that takes a string input and returns a map of all trigrams from that input
+*/
 map<string, int> trigrams(string input)
 {
-    for (int i = 0; i < input.length() - 3; i+=3)
+    map<string, int> inputTris;
+    for (int j = 0; j < input.length() - 2; j++)
     {
-
+        string trigram = input.substr(j,3);
+// If trigram is already in the map, increment that frequency to avoid adding a repeated trigram
+        for (it = inputTris.begin(); it != inputTris.end(); it++)
+        {
+            if (trigram == it->first)
+                {it->second += 1;}
+        }
+// Otherwise add trigram to map
+        inputTris[trigram] = 1
     }
+    return inputTris;
 }
 
-
+/*
+Function that creates a map of every trigram using ' ' and a-z with frequencies of 0
+*/
 map<string, int> allTris()
 {
     map<string, int> everyTri;
@@ -34,8 +49,8 @@ map<string, int> allTris()
 
     for(int i = 0; i < 19683; i++)
     {
-        string trigram = ""
-
+        string trigram = "";
+// Check if each digit needs to be incremented
         if (firstDig == 27 && secDig == 27 && thirdDig == 27)
             {break;}
         else if (secDig == 27 && thirdDig == 27)
@@ -52,7 +67,8 @@ map<string, int> allTris()
         else
         {
             trigram += alphabet[firstDig] + alphabet[secDig] + alphabet[thirdDig];
-        }        
+            thirdDig += 1;
+        }
         everyTri[trigram] = 0
     }
     return everyTri;
--- main.cpp
+++ main.cpp
@@ -3,15 +3,13 @@
 #include <string>
 #include <map>
 
-using namespace ;
+using namespace std;
 
 int main(int argc, char *argv[])
 {
     if (argc != 2){exit(EXIT_FAILURE);}
     map<string, int> inTris = trigrams(argv[1])
     map<string, int> everyTri = allTris()
-
-    int freq = frequencies(inTris, everyTri);
+    string freq = frequencies(inTris, everyTri);
     cout << freq << endl;
-
 }
