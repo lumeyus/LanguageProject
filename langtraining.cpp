becc048547d10c13a82353761c982992e4d4ac97
Unknown <luisfibanez@my.uri.edu>
2017-12-03 22:49:18 -0500

    Improved langtraining design, altered main to match new design

--- langtraining.cpp
+++ langtraining.cpp
@@ -8,8 +8,8 @@ using namespace std;
 // Map of alphabet to help make map of all trigrams
 map<int, char> alphabet;
 alphabet[0] = ' ';
-for (i = 1; i < 27; i++)
-    alphabet[i] = 'a' + (i - 1);
+for (i = 0; i < 26; i++)
+    alphabet[i] = 'a' + i;
 
 // Iterator map to loop through trigram map of input
 map<string, int>::iterator it;
@@ -17,22 +17,13 @@ map<string, int>::iterator it;
 /*
 Function that takes a string input and returns a map of all trigrams from that input
 */
-map<string, int> trigrams(string input)
+void trigrams(string input, map<string, int> &allTrigrams)
 {
-    map<string, int> inputTris;
     for (int j = 0; j < input.length() - 2; j++)
     {
         string trigram = input.substr(j,3);
-// If trigram is already in the map, increment that frequency to avoid adding a repeated trigram
-        for (it = inputTris.begin(); it != inputTris.end(); it++)
-        {
-            if (trigram == it->first)
-                {it->second += 1;}
-        }
-// Otherwise add trigram to map
-        inputTris[trigram] = 1
+        allTrigrams[trigram] += 1;
     }
-    return inputTris;
 }
 
 /*
@@ -42,34 +33,17 @@ map<string, int> allTris()
 {
     map<string, int> everyTri;
 
-    // Digit for each char in trigram
-    int firstDig = 0;
-    int secDig = 0;
-    int thirdDig = 0;
-
-    for(int i = 0; i < 19683; i++)
+    for (int i = 0; i < 27; i++)
     {
-        string trigram = "";
-// Check if each digit needs to be incremented, otherwise add characters using alphabet map to trigram and store
-        if (firstDig == 27 && secDig == 27 && thirdDig == 27)
-            {break;}
-        else if (secDig == 27 && thirdDig == 27)
+        string trigram1 = alphabet[i];
+        for (int j = 0; j < 27; j++)
         {
-            firstDig += 1;
-            secDig = 0;
-            thirdDig = 0;
+            string trigram2 = trigram1 + alphabet[j];
+            for (int k = 0; k < 27; k++)
+                {
+                    string trigram3 = trigram2 + alphabet[k];
+                    everyTri[trigram3] = 0;
+                }
         }
-        else if (thirdDig == 27)
-        {
-            secDig += 1;
-            thirdDig = 0;
-        }
-        else
-        {
-            trigram += alphabet[firstDig] + alphabet[secDig] + alphabet[thirdDig];
-            thirdDig += 1;
-        }
-        everyTri[trigram] = 0
     }
-    return everyTri;
 }
--- main.cpp
+++ main.cpp
@@ -8,8 +8,13 @@ using namespace std;
 int main(int argc, char *argv[])
 {
     if (argc != 2){exit(EXIT_FAILURE);}
-    map<string, int> inTris = trigrams(argv[1]);
     map<string, int> everyTri = allTris();
-    string freq = frequencies(inTris, everyTri);
-    cout << freq << endl;
+    trigrams(argv[1], everyTri);
+
+    map<string, int>::iterator it;
+    for (it = everyTri.begin(); it != everyTri.end(); it++)
+    {
+        cout << it->second << " ";
+    }
+    cout << endl;
 }
